7c45c693dcf3e08ade7a94ccd2a568e110ac56ea
Benjamin Doherty <bjd@bangpound.org>
2014-09-10 19:46:46 -0500

    remove session from client.

--- src/Bangpound/Bundle/InstagramBundle/Client.php
+++ src/Bangpound/Bundle/InstagramBundle/Client.php
@@ -3,22 +3,19 @@
 namespace Bangpound\Bundle\InstagramBundle;
 
 use Instagram\Net\ClientInterface;
-use Symfony\Component\HttpFoundation\Session\Session;
 
 class Client implements ClientInterface
 {
     private $client;
 
-    public function __construct(\GuzzleHttp\Client $client, Session $session)
+    public function __construct(\GuzzleHttp\Client $client)
     {
         $this->client = $client;
-        $this->session = $session;
     }
 
     public function get($url, array $data = null)
     {
         $response = $this->client->get(sprintf("%s?%s", $url, http_build_query($data)));
-        $this->session->set('api_limit', $response->getHeader('X-Ratelimit-Remaining'));
 
         return $response->getBody();
     }
